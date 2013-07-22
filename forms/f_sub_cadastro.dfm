cc9f080e2aa7d8179fa50b53cdb27e40991f8044
Saulo <programeros@gmail.com>
2013-07-22 19:18:32 -0300

    Cadastro de subcadastros - Saulo

---
+++ forms/f_sub_cadastro.dfm
@@ -0,0 +1,5 @@
+inherited frm_form_cadastro_base2: Tfrm_form_cadastro_base2
+  Caption = 'frm_form_cadastro_base2'
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ forms/f_sub_cadastro.pas
@@ -0,0 +1,26 @@
+unit f_sub_cadastro;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, f_cadastro_base, Vcl.Menus, Data.DB,
+  ZAbstractRODataset, ZAbstractDataset, ZDataset, Vcl.Grids, Vcl.DBGrids,
+  rkGlassButton, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Imaging.jpeg;
+
+type
+  Tfrm_form_cadastro_base2 = class(Tfrm_form_cadastro_base)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frm_form_cadastro_base2: Tfrm_form_cadastro_base2;
+
+implementation
+
+{$R *.dfm}
+
+end.
